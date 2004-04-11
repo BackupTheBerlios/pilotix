09d770589ab989a9bc8758e6377d4b263e93c7a2
fluon <fluon>
2004-04-11 10:32:41 +0000

    ajout de java doc

--- Pilotix/src/org/pilotix/common/Angle.java
+++ Pilotix/src/org/pilotix/common/Angle.java
@@ -1,36 +1,53 @@
 /*
- Pilotix : a multiplayer piloting game.
- Copyright (C) 2003 Pilotix.Org
-
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Pilotix : a multiplayer piloting game. Copyright (C) 2003 Pilotix.Org
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 package org.pilotix.common;
 
+/**
+ * Angle en degree modulo entre 0 et 359
+ */
 public class Angle {
 
     protected int degree;
 
+    /**
+     * creation d'un angle de valeur 0
+     */
     public Angle() {
         degree = 0;
     }
 
+    /**
+     * creation d'un angle avec une valeur passe en parametre
+     * 
+     * @param aDegree
+     *            valeur l'angle
+     */
     public Angle(int aDegree) {
         set(aDegree);
     }
 
+    /**
+     * mise a jour de angle avec une valeur passe en parametre
+     * 
+     * @param aDegree
+     *            valeur l'angle
+     */
     public void set(int aDegree) {
         if (aDegree < 0) {
             degree = aDegree % (-360);
@@ -39,10 +56,21 @@ public class Angle {
         }
     }
 
+    /**
+     * mise a jour de angle avec une valeur d'un autre angle
+     * 
+     * @param anAngle
+     *            un angle
+     */
     public void set(Angle anAngle) {
         degree = anAngle.degree;
     }
 
+    /**
+     * recupere la valeur de l'angle
+     * 
+     * @return un angle
+     */
     public int get() {
         return degree;
     }
--- Pilotix/src/org/pilotix/common/Vector.java
+++ Pilotix/src/org/pilotix/common/Vector.java
@@ -1,20 +1,19 @@
 /*
- Pilotix : a multiplayer piloting game.
- Copyright (C) 2003 Pilotix.Org
-
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Pilotix : a multiplayer piloting game. Copyright (C) 2003 Pilotix.Org
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 /*
@@ -27,32 +26,58 @@
 package org.pilotix.common;
 
 /**
- * This class defines a basic type to handle 2D objects caracteristics.
- * 
- * @see PilotixElement
- * 
- * @author Florent Sithimolada
+ * vecteur 2D a multiple usage : position vitesse acceleration
  */
 public class Vector {
 
     public int x;
+
     public int y;
 
+    /**
+     * creation d'un vecteur avec deux valeur initial
+     * 
+     * @param px
+     *            valeur x
+     * @param py
+     *            valeur y 
+     */
     public Vector(int px, int py) {
         x = px;
         y = py;
     }
-
+    /**
+     * mise a jour d'un vecteur avec deux valeur initial
+     * 
+     * @param px
+     *            valeur x
+     * @param py
+     *            valeur y 
+     */
     public void set(int px, int py) {
         x = px;
         y = py;
     }
-
+    /**
+     * mise a jour d'un vecteur avec un autre vecteur
+     * 
+     * @param px
+     *            valeur x
+     * @param py
+     *            valeur y 
+     */
     public void set(Vector b) {
         x = b.x;
         y = b.y;
     }
-
+    /**
+     * produit  vectoriel entre 2 vecteur
+     * 
+     * @param px
+     *            valeur x
+     * @param py
+     *            valeur y 
+     */
     public void dot(Vector b) {
         x = x * b.x;
         y = y * b.y;
