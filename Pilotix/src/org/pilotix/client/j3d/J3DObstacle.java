5ffbb234c36ccc0d425f1983fa429a361391507b
gcolbert <gcolbert>
2004-04-19 17:09:33 +0000

    - essais pour renvoyer la lumiere (pas trop fructueux)

--- Pilotix/src/org/pilotix/client/j3d/J3DObstacle.java
+++ Pilotix/src/org/pilotix/client/j3d/J3DObstacle.java
@@ -22,12 +22,24 @@ package org.pilotix.client.j3d;
 import org.pilotix.common.Vector;
 import org.pilotix.client.Environment;
 import org.pilotix.client.ResourceLocator;
-import com.sun.j3d.utils.geometry.Box;
+
 import javax.media.j3d.Appearance;
-import com.sun.j3d.utils.image.TextureLoader;
 import javax.media.j3d.ImageComponent2D;
 import javax.media.j3d.Texture2D;
 import javax.media.j3d.Texture;
+import javax.media.j3d.QuadArray;
+import javax.media.j3d.GeometryArray;
+import javax.media.j3d.Material;
+import javax.media.j3d.Shape3D;
+import javax.media.j3d.TransparencyAttributes;
+import javax.media.j3d.PolygonAttributes;
+import javax.vecmath.TexCoord2f;
+import javax.vecmath.Point3f;
+import javax.vecmath.Color3f;
+import com.sun.j3d.utils.image.TextureLoader;
+import com.sun.j3d.utils.geometry.GeometryInfo;
+import com.sun.j3d.utils.geometry.NormalGenerator;
+import com.sun.j3d.utils.geometry.Stripifier;
 
 /**
  * <p>
@@ -37,44 +49,164 @@ import javax.media.j3d.Texture;
  */
 public class J3DObstacle extends J3DObject {
 
+    int nbFaces = 5; // On ne dessine pas la face cachee de l'obstacle
+
     /**
      * Construit un obstacle dont les coordonnées des coins sont fournis
      * (le système de coordonnées est celui du serveur), et recouvre
-     * les faces avec la texture indiquée, dont l'URL sera trouvée
+     * les faces avec les textures indiquées, dont les URLs seront trouvées
      * par ResourceLocator.
      */
     public J3DObstacle(Vector upLeftCorner,
                         Vector downRightCorner,
                         int height,
                         int altitude,
-                        String texture) {
+                        String topTexture,
+                        String sideTexture) {
 
         super();
 
+        // --------------------
+        // GEOMETRIE : on réserve 4*nbFaces points
+        QuadArray quadArray = new QuadArray(4*nbFaces,
+                   GeometryArray.COORDINATES
+                   | GeometryArray.TEXTURE_COORDINATE_2
+                   | GeometryArray.NORMALS
+                   | GeometryArray.COLOR_3);
+
+        // Les points, nommés selon leur position par rapport à l'observateur
+        // AuSol signifie z = 0.0f
+        // EnAlt signifie en altitude, z = height*Environment.u3d
+        Point3f pointBasGaucheAuSol = new Point3f(0.0f,
+                                           0.0f,
+                                           0.0f);
+        Point3f pointBasGaucheEnAlt = new Point3f(0.0f,
+                                               0.0f,
+                                               height*Environment.u3d);
+        Point3f pointBasDroiteAuSol = new Point3f((downRightCorner.x-upLeftCorner.x)*Environment.u3d,
+                                           0.0f,
+                                           0.0f);
+        Point3f pointBasDroiteEnAlt = new Point3f((downRightCorner.x-upLeftCorner.x)*Environment.u3d,
+                                               0.0f,
+                                               height*Environment.u3d);
+        Point3f pointHautDroiteAuSol = new Point3f((downRightCorner.x-upLeftCorner.x)*Environment.u3d,
+                                             (upLeftCorner.y-downRightCorner.y)*Environment.u3d,
+                                             0.0f);
+        Point3f pointHautDroiteEnAlt = new Point3f((downRightCorner.x-upLeftCorner.x)*Environment.u3d,
+                                             (upLeftCorner.y-downRightCorner.y)*Environment.u3d,
+                                             height*Environment.u3d);
+        Point3f pointHautGaucheAuSol = new Point3f(0.0f,
+                                           (upLeftCorner.y-downRightCorner.y)*Environment.u3d,
+                                           0.0f);
+        Point3f pointHautGaucheEnAlt = new Point3f(0.0f,
+                                           (upLeftCorner.y-downRightCorner.y)*Environment.u3d,
+                                           height*Environment.u3d);
+
+        // --------------------
+        // FACE 1 : LE DESSUS DE L'OBSTACLE
+        quadArray.setCoordinate(0, pointBasGaucheEnAlt);
+        quadArray.setCoordinate(1, pointBasDroiteEnAlt);
+        quadArray.setCoordinate(2, pointHautDroiteEnAlt);
+        quadArray.setCoordinate(3, pointHautGaucheEnAlt);
+
+        // --------------------
+        // FACE 2 : FACE DU SUD
+        quadArray.setCoordinate(4, pointBasGaucheAuSol);
+        quadArray.setCoordinate(5, pointBasDroiteAuSol);
+        quadArray.setCoordinate(6, pointBasDroiteEnAlt);
+        quadArray.setCoordinate(7, pointBasGaucheEnAlt);
+
+        // --------------------
+        // FACE 3 : FACE DE L'EST
+        quadArray.setCoordinate(8, pointBasDroiteAuSol);
+        quadArray.setCoordinate(9, pointHautDroiteAuSol);
+        quadArray.setCoordinate(10, pointHautDroiteEnAlt);
+        quadArray.setCoordinate(11, pointBasDroiteEnAlt);
+
+        // --------------------
+        // FACE 4 : FACE DU NORD
+        quadArray.setCoordinate(12, pointHautDroiteAuSol);
+        quadArray.setCoordinate(13, pointHautGaucheAuSol);
+        quadArray.setCoordinate(14, pointHautGaucheEnAlt);
+        quadArray.setCoordinate(15, pointHautDroiteEnAlt);
+
+        // --------------------
+        // FACE 5 : FACE DE L'OUEST
+        quadArray.setCoordinate(16, pointHautGaucheAuSol);
+        quadArray.setCoordinate(17, pointBasGaucheAuSol);
+        quadArray.setCoordinate(18, pointBasGaucheEnAlt);
+        quadArray.setCoordinate(19, pointHautGaucheEnAlt);
+
+        // --------------------
+        // TEXTURES : sommet de l'obstacle
         TextureLoader loader = new TextureLoader(
-               Environment.theRL.getResource(ResourceLocator.TEXTURE, texture),
+               Environment.theRL.getResource(ResourceLocator.TEXTURE, topTexture),
                Environment.theGUI);
-        ImageComponent2D image = loader.getImage();
+        ImageComponent2D topImage = loader.getImage();
         Texture2D texture2D = new Texture2D(Texture.BASE_LEVEL,
-                                            Texture.INTENSITY,
-                                            256, 256);
+                                            Texture.RGB,
+                                            topImage.getWidth(),
+                                            topImage.getHeight());
         texture2D.setBoundaryModeS(Texture.WRAP);
         texture2D.setBoundaryModeT(Texture.WRAP);
-        texture2D.setImage(0, image);
+        texture2D.setImage(0, topImage);
 
+        // Définition des points d'accrochage de la texture
+        TexCoord2f tmpTexCoord2f = new TexCoord2f();
+        float x_sur_y = ((downRightCorner.x - upLeftCorner.x)*Environment.u3d)
+                        / ((downRightCorner.y - upLeftCorner.y)*Environment.u3d);
+        int nbRepetitions = 1;
+
+        for (int i=0; i < nbFaces; i++) {
+            tmpTexCoord2f.set(0.0f, nbRepetitions*x_sur_y);
+            quadArray.setTextureCoordinate(0, i*4, tmpTexCoord2f);
+            tmpTexCoord2f.set(0.0f, 0.0f);
+            quadArray.setTextureCoordinate(0, i*4+1, tmpTexCoord2f);
+            tmpTexCoord2f.set(nbRepetitions*1.0f, 0.0f);
+            quadArray.setTextureCoordinate(0, i*4+2, tmpTexCoord2f);
+            tmpTexCoord2f.set(nbRepetitions*1.0f, nbRepetitions*x_sur_y);
+            quadArray.setTextureCoordinate(0, i*4+3, tmpTexCoord2f);
+            quadArray.setColor(i*4, new Color3f(1.0f,0.0f,0.0f));
+            quadArray.setColor(i*4+1, new Color3f(0.0f,1.0f,0.0f));
+            quadArray.setColor(i*4+2, new Color3f(0.0f,0.0f,1.0f));
+            quadArray.setColor(i*4+3, new Color3f(1.0f,1.0f,0.0f));
+        }
+
+        // --------------------
+        // APPARENCE
+        // Création de l'apparence et application des textures
         Appearance obstacleAppearance = new Appearance();
-        obstacleAppearance.setTexture(texture2D);
+//        obstacleAppearance.setTexture(texture2D);
 
-        Box obstacleShape = new Box(((downRightCorner.x - upLeftCorner.x) / 2)*Environment.u3d,
-                                     ((upLeftCorner.y - downRightCorner.y) / 2)*Environment.u3d,
-                                     height*Environment.u3d,
-                                     obstacleAppearance);
-        rotationTG.addChild(obstacleShape);
+        // Material
+        Material material = new Material();
+        material.setColorTarget(Material.EMISSIVE);
+        obstacleAppearance.setMaterial(material);
 
-        this.setPosition(new Vector(
-                upLeftCorner.x + ((downRightCorner.x - upLeftCorner.x) / 2),
-                downRightCorner.y + ((upLeftCorner.y - downRightCorner.y) / 2)));
+        // Transparence
+        TransparencyAttributes transparency = new TransparencyAttributes();
+        transparency.setTransparencyMode(TransparencyAttributes.NONE);
+        transparency.setTransparency(0.0f);
+        obstacleAppearance.setTransparencyAttributes(transparency);
 
+        // Création du Shape3D
+        Shape3D obstacleShape3D = new Shape3D();
+        obstacleShape3D.setAppearance(obstacleAppearance);
+
+        // Calcul des normales
+        GeometryInfo geometryInfo = new GeometryInfo(quadArray);
+//        geometryInfo.convertToIndexedTriangles();
+        NormalGenerator normalGenerator = new NormalGenerator(0.01f);
+        normalGenerator.generateNormals(geometryInfo);
+//        Stripifier stripifier = new Stripifier();
+//        stripifier.stripify(geometryInfo);
+        obstacleShape3D.setGeometry(geometryInfo.getGeometryArray());
+
+        // --------------------
+        // POSITIONNEMENT DE L'OBJET DANS LA SCENE
+        rotationTG.addChild(obstacleShape3D);
+        this.setPosition(new Vector(upLeftCorner.x,
+                                     downRightCorner.y));
         this.setAltitude(altitude);
 
         this.compile();
