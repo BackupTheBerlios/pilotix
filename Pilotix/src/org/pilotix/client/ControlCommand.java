49de226650095e1fd47b79883afd64aff94384b5
masklinn <masklinn>
2004-05-25 20:05:42 +0000

    Modification du fichier de config utilisateur

--- Pilotix/src/org/pilotix/client/ControlCommand.java
+++ Pilotix/src/org/pilotix/client/ControlCommand.java
@@ -1,9 +1,22 @@
 /*
- * Created on 20 mai 2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ Pilotix : a multiplayer piloting game.
+ Copyright (C) 2003 Pilotix.Org
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
+
 package org.pilotix.client;
 
 import java.util.HashMap;
@@ -29,7 +42,7 @@ public class ControlCommand {
 	 * Constructeur : récupère les codes des touches définis dans le fichier de configuration.
 	 */
 	public ControlCommand () {
-		HashMap keys = Environment.userConfig.getBuiltInVars("keymap");
+		HashMap keys = Environment.userConfig.getKeymap();
 		
 		// récupération des touches définies
 		try {
