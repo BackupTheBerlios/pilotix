2a69a364d7fb98dcc25aae580c39ce646cd4d322
gcolbert <gcolbert>
2004-06-30 07:51:39 +0000

    - suppression des tabulations (grrrrrr)

--- Pilotix/src/org/pilotix/client/ControlCommand.java
+++ Pilotix/src/org/pilotix/client/ControlCommand.java
@@ -31,67 +31,67 @@ import org.pilotix.common.*;
  * @see Command
  */
 public class ControlCommand {
-	/** code de la touche d'acceleration */
-	private int accelKey;
-	/** code de la touche d'action */
-	private int useKey;
-	/** Objet Command positionné à partir de l'état des touches */
-	private Command cmd;
-	/** Angle de rotation du vaisseau */
-	private Angle angle;
-	
-	/**
-	 * Constructeur : récupère les codes des touches définis dans le fichier de configuration.
-	 */
-	public ControlCommand () {
-		HashMap keys = Environment.userConfig.getKeymap();
-		
-		// récupération des touches définies
-		try {
-			accelKey = getKeyCode((String)keys.get("ACCELERATE"));
-			useKey = getKeyCode((String)keys.get("USE"));
-			
-		} catch (Exception e) {
-			System.err.println("Erreur lors de la récupération de la config des touches");
-			e.printStackTrace();
-		}
-		
-		cmd = new Command();
-		angle = new Angle();
-	}
-	
-	/**
-	 * Positionne et renvoie l'objet Command à envoyer au serveur à partir de l'état des touches.
+    /** code de la touche d'acceleration */
+    private int accelKey;
+    /** code de la touche d'action */
+    private int useKey;
+    /** Objet Command positionné à partir de l'état des touches */
+    private Command cmd;
+    /** Angle de rotation du vaisseau */
+    private Angle angle;
+    
+    /**
+     * Constructeur : récupère les codes des touches définis dans le fichier de configuration.
+     */
+    public ControlCommand () {
+        HashMap keys = Environment.userConfig.getKeymap();
+        
+        // récupération des touches définies
+        try {
+            accelKey = getKeyCode((String)keys.get("ACCELERATE"));
+            useKey = getKeyCode((String)keys.get("USE"));
+            
+        } catch (Exception e) {
+            System.err.println("Erreur lors de la récupération de la config des touches");
+            e.printStackTrace();
+        }
+        
+        cmd = new Command();
+        angle = new Angle();
+    }
+    
+    /**
+     * Positionne et renvoie l'objet Command à envoyer au serveur à partir de l'état des touches.
      * @return L'objet Command positionné
-	 */
-	public Command getCommand() {
-		int[] keyStatus = Environment.theControls.getKeyStatus();
+     */
+    public Command getCommand() {
+        int[] keyStatus = Environment.theControls.getKeyStatus();
 
-		cmd.setAcceleration(0);
-		cmd.setToolId(0);
-		cmd.setBallId(0);
-		
-		// actions des touches
-		if (keyStatus[accelKey] == Controls.PRESSED) cmd.setAcceleration(2);
-		if (keyStatus[useKey] == Controls.PRESSED) cmd.setToolId(1);
-		
-		// actions de la souris
-		angle.set(Environment.theControls.getMouseVariation().x);
-		cmd.setDirection(angle);
-		
-		return cmd;
-	}
-	
-	
-	/**
-	 * Effectue la correspondance entre un intutilé de touche et son code (voir KeyEvent).
-	 * Malheureusement cette méthode n'existe pas dans KeyEvent... Je n'ai trouvé que ce
-	 * moyen de faire mais c'est pas terrible...
-	 * @param touche
-	 * @return code correspondant.
-	 */
-	
-	private int getKeyCode(String key) throws Exception {
-		return ((Class.forName("java.awt.event.KeyEvent")).getField(key)).getInt(null);
-	}
+        cmd.setAcceleration(0);
+        cmd.setToolId(0);
+        cmd.setBallId(0);
+        
+        // actions des touches
+        if (keyStatus[accelKey] == Controls.PRESSED) cmd.setAcceleration(2);
+        if (keyStatus[useKey] == Controls.PRESSED) cmd.setToolId(1);
+        
+        // actions de la souris
+        angle.set(Environment.theControls.getMouseVariation().x);
+        cmd.setDirection(angle);
+        
+        return cmd;
+    }
+    
+    
+    /**
+     * Effectue la correspondance entre un intutilé de touche et son code (voir KeyEvent).
+     * Malheureusement cette méthode n'existe pas dans KeyEvent... Je n'ai trouvé que ce
+     * moyen de faire mais c'est pas terrible...
+     * @param touche
+     * @return code correspondant.
+     */
+    
+    private int getKeyCode(String key) throws Exception {
+        return ((Class.forName("java.awt.event.KeyEvent")).getField(key)).getInt(null);
+    }
 }
