be5756c0ea4c890905e68a0ed7a2b035e84cba00
gcolbert <gcolbert>
2004-06-30 07:47:29 +0000

    - renommage _ships vers ships

--- Pilotix/src/org/pilotix/server/ServerArea.java
+++ Pilotix/src/org/pilotix/server/ServerArea.java
@@ -31,296 +31,296 @@ import org.w3c.dom.NodeList;
 
 public class ServerArea extends Area {
 
-	//private LinkedList ships = new LinkedList();
-	private LinkedList balls = new LinkedList();
-	private ServerShip tmpShip2;
+    //private LinkedList ships = new LinkedList();
+    private LinkedList balls = new LinkedList();
+    private ServerShip tmpShip2;
 
-	private LinkedList obstacles = new LinkedList();
+    private LinkedList obstacles = new LinkedList();
 
-	//private byte[] byteCoded;
-	//private int lengthInByte;
+    //private byte[] byteCoded;
+    //private int lengthInByte;
 
-	Obstacle borders = new Obstacle(new Vector(0, 65535), new Vector(65535, 0));
+    Obstacle borders = new Obstacle(new Vector(0, 65535), new Vector(65535, 0));
 
-	Vector currentPosition;
-	Vector returnedPosition;
-	Vector returnedSpeed;
-	Vector tmpVector = new Vector();
-	int up;
-	int down;
-	int left;
-	int right;
-	private int radius;
-	private ServerBall tmpBall;
+    Vector currentPosition;
+    Vector returnedPosition;
+    Vector returnedSpeed;
+    Vector tmpVector = new Vector();
+    int up;
+    int down;
+    int left;
+    int right;
+    private int radius;
+    private ServerBall tmpBall;
 
-	public ServerArea(String aMapFile) {
-		super();
-		setMap(aMapFile);
-		//System.out.println("[Area Created]");
+    public ServerArea(String aMapFile) {
+        super();
+        setMap(aMapFile);
+        //System.out.println("[Area Created]");
 
-	}
+    }
 
-	public void setMap(String aMapFile) {
+    public void setMap(String aMapFile) {
 
-		Document document = PilotixServer.theXH
-				.getDocumentFromURL(PilotixServer.theRL.getResource(
-						ResourceLocator.AREA, aMapFile));
-		Element rootNode = document.getDocumentElement();
+        Document document = PilotixServer.theXH
+                .getDocumentFromURL(PilotixServer.theRL.getResource(
+                        ResourceLocator.AREA, aMapFile));
+        Element rootNode = document.getDocumentElement();
 
-		borders.upLeftCorner.x = 0;
-		borders.upLeftCorner.y = Integer.parseInt(rootNode
-				.getAttribute("height"));
-		borders.downRightCorner.x = Integer.parseInt(rootNode
-				.getAttribute("width"));
-		borders.downRightCorner.y = 0;
+        borders.upLeftCorner.x = 0;
+        borders.upLeftCorner.y = Integer.parseInt(rootNode
+                .getAttribute("height"));
+        borders.downRightCorner.x = Integer.parseInt(rootNode
+                .getAttribute("width"));
+        borders.downRightCorner.y = 0;
 
-		NodeList theObstacles = rootNode.getElementsByTagName("Obstacle");
+        NodeList theObstacles = rootNode.getElementsByTagName("Obstacle");
 
-		NodeList tmp;
+        NodeList tmp;
 
-		Element tmpXmlObstacle = null;
+        Element tmpXmlObstacle = null;
 
-		for (int i = 0; i < theObstacles.getLength(); i++) {
-			tmpXmlObstacle = (Element) theObstacles.item(i);
-			obstacles.add(new Obstacle(new Vector(Integer
-					.parseInt(tmpXmlObstacle.getAttribute("upLeftCornerX")),
-					Integer.parseInt(tmpXmlObstacle
-							.getAttribute("upLeftCornerY"))), new Vector(
-					Integer.parseInt(tmpXmlObstacle
-							.getAttribute("downRightCornerX")), Integer
-							.parseInt(tmpXmlObstacle
-									.getAttribute("downRightCornerY")))));
-		}
+        for (int i = 0; i < theObstacles.getLength(); i++) {
+            tmpXmlObstacle = (Element) theObstacles.item(i);
+            obstacles.add(new Obstacle(new Vector(Integer
+                    .parseInt(tmpXmlObstacle.getAttribute("upLeftCornerX")),
+                    Integer.parseInt(tmpXmlObstacle
+                            .getAttribute("upLeftCornerY"))), new Vector(
+                    Integer.parseInt(tmpXmlObstacle
+                            .getAttribute("downRightCornerX")), Integer
+                            .parseInt(tmpXmlObstacle
+                                    .getAttribute("downRightCornerY")))));
+        }
 
-		_ships = new IterableArray(8);
-	}
+        ships = new IterableArray(8);
+    }
 
-	public void addShip(ServerShip aShip) {
-		_ships.add(aShip.getId(), aShip);
-	}
+    public void addShip(ServerShip aShip) {
+        ships.add(aShip.getId(), aShip);
+    }
 
-	public void removeShip(ServerShip aShip) {
-		_ships.remove(aShip.getId());
-	}
+    public void removeShip(ServerShip aShip) {
+        ships.remove(aShip.getId());
+    }
 
-	public void nextFrame() {
+    public void nextFrame() {
 
-		//creation des differents projectils :
-		/*
-		 * for (int i = 0; i < ships.size(); i++) { tmpShip = (ServerShip)
-		 * ships.get(i); tmpShip.commandPilotixElement(balls); }
-		 */
+        //creation des differents projectils :
+        /*
+         * for (int i = 0; i < ships.size(); i++) { tmpShip = (ServerShip)
+         * ships.get(i); tmpShip.commandPilotixElement(balls); }
+         */
 
-		//Calcule de prochaine trajectoirs des balls sans collisions:
-		/*
-		 * for (int i = 0; i < balls.size(); i++) { tmpBall = (ServerBall)
-		 * balls.get(i); tmpBall.computeSpeedFromForces(); }
-		 */
+        //Calcule de prochaine trajectoirs des balls sans collisions:
+        /*
+         * for (int i = 0; i < balls.size(); i++) { tmpBall = (ServerBall)
+         * balls.get(i); tmpBall.computeSpeedFromForces(); }
+         */
 
-		//Calcule de prochaine trajectoirs des ships sans collisions:
-		for (_ships.setCursorOnFirst(); _ships.hasNext();) {
-			tmpShip2 = (ServerShip) _ships.next();
-			tmpShip2.computeSpeedFromForces();
-		}
+        //Calcule de prochaine trajectoirs des ships sans collisions:
+        for (ships.setCursorOnFirst(); ships.hasNext();) {
+            tmpShip2 = (ServerShip) ships.next();
+            tmpShip2.computeSpeedFromForces();
+        }
 
-		//modification des trajectoires en prenant en compte les collisions
-		// ships/balls
-		/*
-		 * for (int i = 0; i < ships.size(); i++) { tmpShip = (ServerShip)
-		 * ships.get(i); collideWithBalls(tmpShip); }
-		 */
+        //modification des trajectoires en prenant en compte les collisions
+        // ships/balls
+        /*
+         * for (int i = 0; i < ships.size(); i++) { tmpShip = (ServerShip)
+         * ships.get(i); collideWithBalls(tmpShip); }
+         */
 
-		//modification des trajectoires en prenant en compte les collisions
-		// ships/ships
-		for (_ships.setCursorOnFirst(); _ships.hasNext();) {
-			tmpShip2 = (ServerShip) _ships.next();
-			collideWithShips(tmpShip2);
-		}
+        //modification des trajectoires en prenant en compte les collisions
+        // ships/ships
+        for (ships.setCursorOnFirst(); ships.hasNext();) {
+            tmpShip2 = (ServerShip) ships.next();
+            collideWithShips(tmpShip2);
+        }
 
-		//modification des trajectoires en prenant en compte les collisions
-		//Ships/Obstacles et Ships/frontiere externes
-		for (_ships.setCursorOnFirst(); _ships.hasNext();) {
-			tmpShip2 = (ServerShip) _ships.next();
-			collideWithBoundary(tmpShip2);
-			collideWithObstacle(tmpShip2);
-		}
+        //modification des trajectoires en prenant en compte les collisions
+        //Ships/Obstacles et Ships/frontiere externes
+        for (ships.setCursorOnFirst(); ships.hasNext();) {
+            tmpShip2 = (ServerShip) ships.next();
+            collideWithBoundary(tmpShip2);
+            collideWithObstacle(tmpShip2);
+        }
 
-		// affectation des trajectoirs des ships
-		for (_ships.setCursorOnFirst(); _ships.hasNext();) {
-			tmpShip2 = (ServerShip) _ships.next();
-			tmpShip2.nextFrame();
-		}
+        // affectation des trajectoirs des ships
+        for (ships.setCursorOnFirst(); ships.hasNext();) {
+            tmpShip2 = (ServerShip) ships.next();
+            tmpShip2.nextFrame();
+        }
 
-	}
+    }
 
-	public void collideWithBoundary(ServerShip s) {
+    public void collideWithBoundary(ServerShip s) {
 
-		currentPosition = s.getPosition();
-		returnedPosition = s.getNextPosition();
-		returnedSpeed = s.getNextSpeed();
-		radius = s.getRadius();
+        currentPosition = s.getPosition();
+        returnedPosition = s.getNextPosition();
+        returnedSpeed = s.getNextSpeed();
+        radius = s.getRadius();
 
-		up = borders.upLeftCorner.y;
-		down = borders.downRightCorner.y;
-		left = borders.upLeftCorner.x;
-		right = borders.downRightCorner.x;
+        up = borders.upLeftCorner.y;
+        down = borders.downRightCorner.y;
+        left = borders.upLeftCorner.x;
+        right = borders.downRightCorner.x;
 
-		if (returnedPosition.x < (left + radius)) {
-			returnedPosition.x = left + radius;
-			returnedSpeed.x = 0;
-		} else if (returnedPosition.x > (right - radius)) {
-			returnedPosition.x = right - radius;
-			returnedSpeed.x = 0;
-		}
-		if (returnedPosition.y > (up - radius)) {
-			returnedPosition.y = up - radius;
-			returnedSpeed.y = 0;
-		} else if (returnedPosition.y < (down + radius)) {
-			returnedPosition.y = down + radius;
-			returnedSpeed.y = 0;
-		}
-	}
+        if (returnedPosition.x < (left + radius)) {
+            returnedPosition.x = left + radius;
+            returnedSpeed.x = 0;
+        } else if (returnedPosition.x > (right - radius)) {
+            returnedPosition.x = right - radius;
+            returnedSpeed.x = 0;
+        }
+        if (returnedPosition.y > (up - radius)) {
+            returnedPosition.y = up - radius;
+            returnedSpeed.y = 0;
+        } else if (returnedPosition.y < (down + radius)) {
+            returnedPosition.y = down + radius;
+            returnedSpeed.y = 0;
+        }
+    }
 
-	public void collideWithObstacle(ServerShip s) {
-		currentPosition = s.getPosition();
-		returnedPosition = s.getNextPosition();
-		returnedSpeed = s.getNextSpeed();
-		for (Iterator iter = obstacles.iterator(); iter.hasNext();) {
-			Obstacle obstacle = (Obstacle) iter.next();
-			up = obstacle.upLeftCorner.y;
-			down = obstacle.downRightCorner.y;
-			left = obstacle.upLeftCorner.x;
-			right = obstacle.downRightCorner.x;
-			if (((left - radius) < returnedPosition.x)
-					&& (returnedPosition.x < (right + radius))
-					&& ((down - radius) < returnedPosition.y)
-					&& (returnedPosition.y < (up + radius))) {
-				if (currentPosition.x < left) {
-					returnedSpeed.x = 0;
-					returnedPosition.x = left - radius;
-				} else if (right < currentPosition.x) {
-					returnedSpeed.x = 0;
-					returnedPosition.x = right + radius;
-				}
-				if (currentPosition.y < down) {
-					returnedSpeed.y = 0;
-					returnedPosition.y = down - radius;
-				} else if (up < currentPosition.y) {
-					returnedSpeed.y = 0;
-					returnedPosition.y = up + radius;
-				}
-			}
-		}
-	}
+    public void collideWithObstacle(ServerShip s) {
+        currentPosition = s.getPosition();
+        returnedPosition = s.getNextPosition();
+        returnedSpeed = s.getNextSpeed();
+        for (Iterator iter = obstacles.iterator(); iter.hasNext();) {
+            Obstacle obstacle = (Obstacle) iter.next();
+            up = obstacle.upLeftCorner.y;
+            down = obstacle.downRightCorner.y;
+            left = obstacle.upLeftCorner.x;
+            right = obstacle.downRightCorner.x;
+            if (((left - radius) < returnedPosition.x)
+                    && (returnedPosition.x < (right + radius))
+                    && ((down - radius) < returnedPosition.y)
+                    && (returnedPosition.y < (up + radius))) {
+                if (currentPosition.x < left) {
+                    returnedSpeed.x = 0;
+                    returnedPosition.x = left - radius;
+                } else if (right < currentPosition.x) {
+                    returnedSpeed.x = 0;
+                    returnedPosition.x = right + radius;
+                }
+                if (currentPosition.y < down) {
+                    returnedSpeed.y = 0;
+                    returnedPosition.y = down - radius;
+                } else if (up < currentPosition.y) {
+                    returnedSpeed.y = 0;
+                    returnedPosition.y = up + radius;
+                }
+            }
+        }
+    }
 
-	class Obstacle {
+    class Obstacle {
 
-		public Vector upLeftCorner;
-		public Vector downRightCorner;
+        public Vector upLeftCorner;
+        public Vector downRightCorner;
 
-		public Obstacle(Vector upLeftCorner, Vector downRightCorner) {
-			this.upLeftCorner = upLeftCorner;
-			this.downRightCorner = downRightCorner;
-		}
+        public Obstacle(Vector upLeftCorner, Vector downRightCorner) {
+            this.upLeftCorner = upLeftCorner;
+            this.downRightCorner = downRightCorner;
+        }
 
-	}
+    }
 
-	private void collideWithBalls(ServerShip aShip) {
-		for (int i = 0; i < balls.size(); i++) {
-			tmpBall = (ServerBall) balls.get(i);
-			collideWithBall(tmpShip2, tmpBall);
-		}
+    private void collideWithBalls(ServerShip aShip) {
+        for (int i = 0; i < balls.size(); i++) {
+            tmpBall = (ServerBall) balls.get(i);
+            collideWithBall(tmpShip2, tmpBall);
+        }
 
-	}
+    }
 
-	private void collideWithBall(ServerShip aShip, ServerBall aBall) {
-		Vector va = aShip.getNextPosition().less(aShip.getPosition());
-		Vector vb = aBall.getNextPosition().less(aBall.getPosition());
-		Vector AB = aBall.getPosition().less(aShip.getPosition());
-		Vector vab = vb.less(va);
-		int rab = aShip.getRadius() + aBall.getRadius();
+    private void collideWithBall(ServerShip aShip, ServerBall aBall) {
+        Vector va = aShip.getNextPosition().less(aShip.getPosition());
+        Vector vb = aBall.getNextPosition().less(aBall.getPosition());
+        Vector AB = aBall.getPosition().less(aShip.getPosition());
+        Vector vab = vb.less(va);
+        int rab = aShip.getRadius() + aBall.getRadius();
 
-		//if (AB.dot(AB) <= rab * rab) {
-		//} else {
-		long a = vab.dot(vab);
-		long b = 2 * vab.dot(AB);
-		long c = (AB.dot(AB)) - (rab * rab);
-		long q = (b * b) - (4 * a * c);
-		if (q >= 0) {
-			double sq = Math.sqrt(q);
-			double d = ((double) 1) / ((double) (2 * a));
-			double r1 = (-b + sq) * d;
-			double r2 = (-b - sq) * d;
-			r1 = Math.min(r1, r2);
-			if ((0 < r1) && (r1 < 1)) {
-				//System.out.println("r1 " + r1);
-				aShip.setNextPosition(aShip.getPosition().plus(va.mult(r1))
-						.plus(vb.mult(1 - r1)));
-				//aBall.setNextPosition(aBall.getPosition().plus(vb.mult(r1)).plus(
-				//    va.mult(1 - r1)));
+        //if (AB.dot(AB) <= rab * rab) {
+        //} else {
+        long a = vab.dot(vab);
+        long b = 2 * vab.dot(AB);
+        long c = (AB.dot(AB)) - (rab * rab);
+        long q = (b * b) - (4 * a * c);
+        if (q >= 0) {
+            double sq = Math.sqrt(q);
+            double d = ((double) 1) / ((double) (2 * a));
+            double r1 = (-b + sq) * d;
+            double r2 = (-b - sq) * d;
+            r1 = Math.min(r1, r2);
+            if ((0 < r1) && (r1 < 1)) {
+                //System.out.println("r1 " + r1);
+                aShip.setNextPosition(aShip.getPosition().plus(va.mult(r1))
+                        .plus(vb.mult(1 - r1)));
+                //aBall.setNextPosition(aBall.getPosition().plus(vb.mult(r1)).plus(
+                //    va.mult(1 - r1)));
 
-				tmpVector.set(aShip.getNextSpeed());
-				aShip.setNextSpeed(aBall.getSpeed());
-				//aBall.setNextSpeed(tmpVector);
+                tmpVector.set(aShip.getNextSpeed());
+                aShip.setNextSpeed(aBall.getSpeed());
+                //aBall.setNextSpeed(tmpVector);
 
-				aBall.setState(ServerBall.TO_DELETE);
+                aBall.setState(ServerBall.TO_DELETE);
 
-			} else {
-				//System.out.println("no collision");
-			}
-		} else {
-			//System.out.println("no collision");
-		}
-		//}
+            } else {
+                //System.out.println("no collision");
+            }
+        } else {
+            //System.out.println("no collision");
+        }
+        //}
 
-	}
+    }
 
-	public void collideWithShips(ServerShip s1) {
-		for (_ships.setCursorOnFirst(); _ships.hasNext();) {
-			tmpShip2 = (ServerShip) _ships.next();
-			if (tmpShip2.getId() > s1.getId()) {
-				collideWithShip(s1, tmpShip2);
-			}
-		}
-	}
+    public void collideWithShips(ServerShip s1) {
+        for (ships.setCursorOnFirst(); ships.hasNext();) {
+            tmpShip2 = (ServerShip) ships.next();
+            if (tmpShip2.getId() > s1.getId()) {
+                collideWithShip(s1, tmpShip2);
+            }
+        }
+    }
 
-	public void collideWithShip(ServerShip sa, ServerShip sb) {
-		Vector va = sa.getNextPosition().less(sa.getPosition());
-		Vector vb = sb.getNextPosition().less(sb.getPosition());
-		Vector AB = sb.getPosition().less(sa.getPosition());
-		Vector vab = vb.less(va);
-		int rab = sa.getRadius() + sb.getRadius();
+    public void collideWithShip(ServerShip sa, ServerShip sb) {
+        Vector va = sa.getNextPosition().less(sa.getPosition());
+        Vector vb = sb.getNextPosition().less(sb.getPosition());
+        Vector AB = sb.getPosition().less(sa.getPosition());
+        Vector vab = vb.less(va);
+        int rab = sa.getRadius() + sb.getRadius();
 
-		//if (AB.dot(AB) <= rab * rab) {
-		//} else {
-		long a = vab.dot(vab);
-		long b = 2 * vab.dot(AB);
-		long c = (AB.dot(AB)) - (rab * rab);
-		long q = (b * b) - (4 * a * c);
-		if (q >= 0) {
-			double sq = Math.sqrt(q);
-			double d = ((double) 1) / ((double) (2 * a));
-			double r1 = (-b + sq) * d;
-			double r2 = (-b - sq) * d;
-			r1 = Math.min(r1, r2);
-			if ((0 < r1) && (r1 < 1)) {
-				//System.out.println("r1 " + r1);
-				sa.setNextPosition(sa.getPosition().plus(va.mult(r1)).plus(
-						vb.mult(1 - r1)));
-				sb.setNextPosition(sb.getPosition().plus(vb.mult(r1)).plus(
-						va.mult(1 - r1)));
+        //if (AB.dot(AB) <= rab * rab) {
+        //} else {
+        long a = vab.dot(vab);
+        long b = 2 * vab.dot(AB);
+        long c = (AB.dot(AB)) - (rab * rab);
+        long q = (b * b) - (4 * a * c);
+        if (q >= 0) {
+            double sq = Math.sqrt(q);
+            double d = ((double) 1) / ((double) (2 * a));
+            double r1 = (-b + sq) * d;
+            double r2 = (-b - sq) * d;
+            r1 = Math.min(r1, r2);
+            if ((0 < r1) && (r1 < 1)) {
+                //System.out.println("r1 " + r1);
+                sa.setNextPosition(sa.getPosition().plus(va.mult(r1)).plus(
+                        vb.mult(1 - r1)));
+                sb.setNextPosition(sb.getPosition().plus(vb.mult(r1)).plus(
+                        va.mult(1 - r1)));
 
-				tmpVector.set(sa.getNextSpeed());
-				sa.setNextSpeed(sb.getNextSpeed());
-				sb.setNextSpeed(tmpVector);
+                tmpVector.set(sa.getNextSpeed());
+                sa.setNextSpeed(sb.getNextSpeed());
+                sb.setNextSpeed(tmpVector);
 
-			} else {
-				//System.out.println("no collision");
-			}
-		} else {
-			//System.out.println("no collision");
-		}
-		//}
-	}
+            } else {
+                //System.out.println("no collision");
+            }
+        } else {
+            //System.out.println("no collision");
+        }
+        //}
+    }
 
 }
